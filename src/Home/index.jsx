f788be42bd5fffe5c2c48692f787b2c14f559efa
Enirose <enirosehellum@gmail.com>
2023-10-13 00:17:26 +0200

    updated styling

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
-import SwiperCarousel from "../Projects";
-import 'swiper/css'
 import Intro from "../MyIntro";
 import AboutMe from "../AboutMe";
 import ContactMe from "../ContactMe";
--- src/NavBar/index.jsx
+++ src/NavBar/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Container, Navbar, Nav, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export default function Navigation() {
   return (
-    <Navbar expand="lg" className='d-flex justify-content-between justify-content-lg-center align-items-center'>
+    <Row>
+    <Navbar expand="lg">
       <Container className='navContainer'>
-        <Navbar.Brand href="/"><b>Eni<font color='#d78500'>Portfolio</font></b></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="text-center text-lg-end">
+        <Navbar.Brand className='logo' href="/"><b><font color='#fff'>Eni</font><font color='#dfb15b'>Portfolio</font></b></Navbar.Brand>
+         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto mb-2 mb-lg-0">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
             <Nav.Link href="#projects">Projects</Nav.Link>
@@ -16,8 +17,9 @@ export default function Navigation() {
             <Nav.Link href="#contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
-      </Container> 
+      </Container>    
     </Navbar>  
+    </Row>
   );
 }
 
